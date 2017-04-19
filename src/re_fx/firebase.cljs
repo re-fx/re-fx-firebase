c99414ba759c41ba80fb0f2925affbd23f06b680
Fabian Aussems <fabianaussems@gmail.com>
2017-04-19 17:08:42 +0200

    added listen-values

--- src/re_fx/firebase.cljs
+++ src/re_fx/firebase.cljs
@@ -110,7 +110,7 @@
   (fn firebase-connected-listen-fx [{:keys [on-connected on-disconnected]
                                      :or   {on-connected [:firebase-connected-listen-no-on-connected]
                                             on-disconnected [:firebase-connected-listen-no-on-disconnected]}}]
-    (let [ref (.ref firebase-db ".info/connected")]
+    (let [ref (.ref (.database root) ".info/connected")]
       (.on ref "value" (fn firebase-connected-listen-fx-value [s]
                          (let [v (js->clj (.val s))]
                            (if v
@@ -119,37 +119,25 @@
       nil)))
 
  (re/reg-fx
-  :firebase-login
-  (fn firebase-login-fx [{:keys [username password on-success on-failure]
-                          :or   {on-success [:firebase-login-no-on-success]
-                                 on-failure [:firebase-login-no-on-failure]}}]
-    (.then (.signInWithEmailAndPassword (.auth root) username password)
-           (fn firebase-login-on-success [] (re/dispatch on-success))
-           (fn firebase-login-on-failure [] (re/dispatch on-failure)))))
-
- (re/reg-fx
-  :firebase-login-with-custom-token
-  (fn firebase-login-with-custom-token-fx [{:keys [token on-success on-failure]
-                                            :or   {on-success [:firebase-login-with-custom-token-no-on-success]
-                                                   on-failure [:firebase-login-with-custom-token-no-on-failure]}}]
-    (.then (.signInWithCustomToken (.auth root) token)
-           (fn firebase-login-with-custom-token-on-success [] (re/dispatch on-success))
-           (fn firebase-login-with-custom-token-on-failure [] (re/dispatch on-failure)))))
-
- (re/reg-fx
-  :firebase-logout
-  (fn firebase-logout-fx [{:keys [on-success on-failure]
-                           :or   {on-success [:firebase-logout-no-on-success]
-                                  on-failure [:firebase-logout-no-on-failure]}}]
-    (.then (.signOut (.auth root))
-           (fn firebase-logout-on-success [] (re/dispatch on-success))
-           (fn firebase-logout-on-failure [] (re/dispatch on-failure)))))
+  :firebase-listen-values
+  (fn firebase-listen-values-fx [ms]
+   (->> ms
+        (map (fn firebase-listen-value-item [[m v]]
+               (let [{:keys [path value-changed]
+                      :or   {path ""
+                             value-changed [(keyword (str "firebase-listen-values-" m "-value-changed-added"))]}} v
+                     ref (.ref (.database root) path)]
+                 (.on ref "value" (fn firebase-listen-value-changed [c]
+                                    (let [vv (.val c)]
+                                      (re/dispatch (conj value-changed vv))))))))
+        (doall))
+   nil))
 
  (re/reg-fx
   :firebase-listen-value
-  (fn firebase-listen-fx [{:keys [path value-changed]
+  (fn firebase-listen-value-fx [{:keys [path value-changed]}]
                            :or {path ""
-                                value-changed [(keyword (str "firebase-listen-value-m-no-value-changed"))]}}]
+                                value-changed [(keyword (str "firebase-listen-value-m-no-value-changed"))]}
     (let [ref (.ref (.database root) path)]
       (.on ref "value" (fn firebase-listen-value-changed [c]
                          (let [vv (.val c)]
